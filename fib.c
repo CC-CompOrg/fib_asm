134ee8415e63e5f51848996fbd4fbc51073e6794
Dan Ellsworth <dellsworth@b4cp275.coloradocollege.edu>
2022-12-08 13:31:57 -0700

    expand the C code a bit

--- fib.c
+++ fib.c
@@ -2,7 +2,14 @@ char fib(char n) {
     if(n<2) {
         return 1;
     }
-    return fib(n-1)+fib(n-2);
+    // This line hides a lot of complexity...
+    // Converting into something that has less
+    // hidden steps
+    //return fib(n-1)+fib(n-2);
+    char a = fib(n-1);
+    char b = fib(n-2);
+    char c = a+b;
+    return c;
 }
 
 void main() {
